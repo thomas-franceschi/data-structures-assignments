c1c13c6e1ad0736359dba450810b24d9cb2b93c6
tfrances <tfrances@nd.edu>
2016-08-31 00:28:44 -0400

    challenge01

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -22,51 +22,58 @@ int main(int argc, char *argv[]) {
     //Vector to be rotated
     vector<int> myVector;
 
-    //cout << "Size of Vector - Shift # - Direction (L/R)" << endl;;
-    cin >> size >> shift >> dir;
+    while(1) {
 
-    //cout << "size: " << size << endl << "shift: " << shift << endl << "Direction: " << dir << endl;
+        //cout << "Size of Vector - Shift # - Direction (L/R)" << endl;;
+        cin >> size >> shift >> dir;
 
-    for ( int i = 0; i < size; i++) {
-        cin >> vecValue;
-        myVector.push_back(vecValue);
-    }
+        //cout << "size: " << size << endl << "shift: " << shift << endl << "Direction: " << dir << endl;
 
-//Left Rotate
-if ( dir == 'L' || dir == 'l') {
-        //Print original order
-        //for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
-        //cout << endl;
+        for ( int i = 0; i < size; i++) {
+            cin >> vecValue;
+            myVector.push_back(vecValue);
+        }
 
-    for (int k = 0; k < shift; k++) {
-            //Rotate vector once
-            for (int j = 0; j < size - 1; j++) {
+    //Left Rotate
+    if ( dir == 'L' || dir == 'l') {
+            //Print original order
+            //for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+            //cout << endl;
 
-                swap( myVector[j], myVector[j+1] );
+        for (int k = 0; k < shift; k++) {
+                //Rotate vector once
+                for (int j = 0; j < size - 1; j++) {
 
-                
+                    swap( myVector[j], myVector[j+1] );
+
+                    
+                }
             }
         }
-    }
 
-    //Right Rotate
-if ( dir == 'R' || dir == 'r') {
-        //Print original order
-        //for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
-        //cout << endl;
+        //Right Rotate
+    if ( dir == 'R' || dir == 'r') {
+            //Print original order
+            //for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+            //cout << endl;
 
-    for (int k = 0; k < shift; k++) {
-            //Rotate vector once
-            for (int j = size - 1; j > 0; j--) {
+        for (int k = 0; k < shift; k++) {
+                //Rotate vector once
+                for (int j = size - 1; j > 0; j--) {
 
-                swap( myVector[j], myVector[j-1] );
+                    swap( myVector[j], myVector[j-1] );
 
-                //for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
-                //cout << endl;
+                    //for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+                    //cout << endl;
+                }
             }
         }
+        //print output
+        for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+        cout << endl;
+
+        //clear vector
+        myVector.clear();
     }
-    for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
-                cout << endl;
 
 }
