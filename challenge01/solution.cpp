693db4237c429c844acfa380f9924e61dd0a36fb
tfrances <tfrances@nd.edu>
2016-08-31 00:20:33 -0400

    challenge01

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -2,8 +2,70 @@
 
 // Main Execution
 
-int main(int argc, char *argv[]) {
-    return 0;
-}
 
-// vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
+//Thomas Franceschi
+//Challenge 01
+
+#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    vector<char> instructions;
+    int size;
+    int shift;
+    int vecValue;
+    char dir;
+    //Vector to be rotated
+    vector<int> myVector;
+
+    cout << "Size of Vector - Shift # - Direction (L/R)" << endl;;
+    cin >> size >> shift >> dir;
+
+    cout << "size: " << size << endl << "shift: " << shift << endl << "Direction: " << dir << endl;
+
+    for ( int i = 0; i < size; i++) {
+        cin >> vecValue;
+        myVector.push_back(vecValue);
+    }
+
+//Left Rotate
+if ( dir == 'L' || dir == 'l') {
+        //Print original order
+        for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+        cout << endl;
+
+    for (int k = 0; k < shift; k++) {
+            //Rotate vector once
+            for (int j = 0; j < size - 1; j++) {
+
+                swap( myVector[j], myVector[j+1] );
+
+                for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+                cout << endl;
+            }
+        }
+    }
+
+    //Right Rotate
+if ( dir == 'R' || dir == 'r') {
+        //Print original order
+        for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+        cout << endl;
+
+    for (int k = 0; k < shift; k++) {
+            //Rotate vector once
+            for (int j = size - 1; j > 0; j--) {
+
+                swap( myVector[j], myVector[j-1] );
+
+                for (auto i = myVector.begin(); i != myVector.end(); ++i) cout << *i << ' ';
+                cout << endl;
+            }
+        }
+    }
+
+}
