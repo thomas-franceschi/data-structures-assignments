cbb0c9395c7e7fac0b73ba3101eb19d05d28f130
tfrances <tfrances@nd.edu>
2016-09-14 10:33:48 -0400

    challenge03

--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -10,7 +10,58 @@ using namespace std;
 
 // Main Execution
 
-int main(int argc, char *argv[]) {
+int main() {
+
+    int num;
+    int temp;
+    int minDiff;
+    int tempDiff;
+    vector<int> myVec;
+    vector<int> pairs;
+
+    for( int test = 0; test < 9; test++){
+        //read in number of integers
+        cin >> num;
+
+        //read in integers
+        for( int n = 0; n < num; n++){
+            cin >> temp;
+            myVec.push_back(temp);
+        }
+
+        //sort list
+        sort(myVec.begin(), myVec.end());
+
+        //compare
+        minDiff = myVec[ 1 ] - myVec[ 0 ];
+        for ( unsigned int i = 0; i < myVec.size() - 1; i++ ){
+            //set diff
+            tempDiff = myVec[ i + 1 ] - myVec[ i ];
+
+            //both positive or both negative
+            if( tempDiff < minDiff) {
+                pairs.clear();
+                pairs.push_back(myVec[i]);
+                pairs.push_back(myVec[i+1]);
+                minDiff = tempDiff;
+            }
+            else if (tempDiff == minDiff) {
+                pairs.push_back(myVec[i]);
+                pairs.push_back(myVec[i+1]); 
+            }
+
+        }
+        
+
+        for ( unsigned int j = 0; j < pairs.size(); j++){
+            cout << pairs[j];
+            if ( j != pairs.size() - 1) cout << ' ';
+        }
+
+            cout << endl;
+            pairs.clear();
+            myVec.clear();
+    }
     return EXIT_SUCCESS;
 }
 
