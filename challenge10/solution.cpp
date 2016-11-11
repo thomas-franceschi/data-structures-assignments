9834fe988c5258224b073e35a63665a4ff513dc9
Peter Bui <pbui@nd.edu>
2016-11-11 00:18:57 -0500

    Add challenge 10

---
+++ challenge10/solution.cpp
@@ -0,0 +1,11 @@
+// Challenge 10: Minimum Spanning Tree
+
+using namespace std;
+
+// Main Execution
+
+int main(int argc, char *argv[]) {
+    return EXIT_SUCCESS;
+}
+
+// vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
